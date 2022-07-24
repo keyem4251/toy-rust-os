115fcdc98608ad88cac92ee15694c07c724a4407
keyem4251 <kisk.emura@gmail.com>
2022-07-24 14:18:26 +0900

    qemu hello world

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 // リンカが_startという関数を探すためエントリポイントを定義
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
