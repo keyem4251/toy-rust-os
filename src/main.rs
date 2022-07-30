a616acfdcb1d2e760a6d3ae4c95b06b4cf50f53a
keyem4251 <kisk.emura@gmail.com>
2022-07-30 14:12:22 +0900

    call something print

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,10 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-
 // リンカが_startという関数を探すためエントリポイントを定義
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
