e66e1526d713b0578bd6f6e96e08d550d0f5dc08
keyem4251 <kisk.emura@gmail.com>
2022-08-02 21:42:39 +0900

    static writer main

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ mod vga_buffer;
 // リンカが_startという関数を探すためエントリポイントを定義
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
