c02172fb95c57b8af5202ed479e395f56f945b17
keyem4251 <kisk.emura@gmail.com>
2022-08-02 21:40:08 +0900

    static writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,18 @@
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe {
+          &mut *(0xb8000 as *mut Buffer)  
+        },
+    });
+}
 
 // 色を定義
 #[allow(dead_code)]
@@ -135,18 +148,4 @@ impl fmt::Write for Writer {
         self.write_string(s);
         Ok(())
     }
-}
-
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("World!");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.8).unwrap();
 }
