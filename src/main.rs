2bd419f08bb930f7d8afe1561e5a6e9c41ae23c3
keyem4251 <kisk.emura@gmail.com>
2022-08-04 21:45:23 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // Rustの標準ライブラリをリンクしない
 #![no_main] // Rustレベルのエントリポイントを無効化
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -9,6 +12,10 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -17,4 +24,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
