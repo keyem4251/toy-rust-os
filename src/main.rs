f0b0df2e04979978ac2be23186722b7d4b8a0ce5
keyem4251 <kisk.emura@gmail.com>
2022-08-08 21:16:59 +0900

    add comment

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     use x86_64::instructions::port::Port;
 
     unsafe {
+        // 0xf4: isa-debug-exitデバイスのiobase
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
