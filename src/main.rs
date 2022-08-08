9121ff8776e514f0e39ad36bba3580065546074b
keyem4251 <kisk.emura@gmail.com>
2022-08-08 21:00:31 +0900

    qemu exit code

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,22 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 // パニック時に呼ばれる関数を定義
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -32,6 +48,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
