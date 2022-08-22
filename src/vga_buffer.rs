9eaa43dee71cd9fdde06f9da1df6af4995e694d9
keyem4251 <kisk.emura@gmail.com>
2022-08-22 22:37:10 +0900

    add vga simple test

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -166,3 +166,8 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
