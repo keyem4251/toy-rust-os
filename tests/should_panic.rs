0671c7fc81422d5488c17a3ccdc7a770a1a4e6ed
keyem4251 <kisk.emura@gmail.com>
2022-08-25 21:34:05 +0900

    disable harness for should panic

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(toy_rust_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use toy_rust_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
-
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -21,17 +19,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
