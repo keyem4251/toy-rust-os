1e90956e5a43f2e0c4c529d35a0cf2fbef9e77f5
keyem4251 <kisk.emura@gmail.com>
2022-09-01 21:13:09 +0900

    raise duble fault

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@ pub extern "C" fn _start() -> ! {
 
     toy_rust_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
