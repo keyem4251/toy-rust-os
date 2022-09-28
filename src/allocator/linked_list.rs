835cf80cb87b05ee7ca9ef4039f2cbcd1e8dbf1c
keyem4251 <kisk.emura@gmail.com>
2022-09-28 21:25:04 +0900

    implement linked list allocator

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,3 +1,24 @@
+pub struct LinkedListAllocator {
+    head: ListNode,
+}
+
+impl LinkedListAllocator {
+    pub const fn new() -> Self {
+        LinkedListAllocator { head: ListNode::new(0) }
+    }
+
+    /// # Safety
+    // 与えられたヒープの位置、サイズで初期化する
+    // 呼び出し側で有効なメモリの範囲を指定する必要があるためunsafe
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.add_free_region(heap_start, heap_size);
+    }
+
+    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        todo!();
+    }
+}
+
 struct ListNode {
     size: usize,
     next: Option<&'static mut ListNode>,
