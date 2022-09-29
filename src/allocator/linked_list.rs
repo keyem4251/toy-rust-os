cfa09da16ef1fc6618c94f73864bd34ee23a8a22
keyem4251 <kisk.emura@gmail.com>
2022-09-29 21:34:47 +0900

    implement global alloc

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,5 +1,7 @@
-use super::align_up;
+use super::{align_up, Locked};
+use alloc::alloc::{GlobalAlloc, Layout};
 use core::mem;
+use core::ptr;
 
 pub struct LinkedListAllocator {
     head: ListNode,
@@ -74,6 +76,13 @@ impl LinkedListAllocator {
         // 割当のための領域確保
         Ok(alloc_start)
     }
+
+    // 受け取ったlayoutをListNodeで保存するために調整する
+    fn size_align(layout: Layout) -> (usize, usize) {
+        let layout = layout.align_to(mem::align_of::<ListNode>()).expect("adjusting alignment failed").pad_to_align();
+        let size = layout.size().max(mem::size_of::<ListNode>());
+        (size, layout.align())
+    }
 }
 
 struct ListNode {
@@ -92,5 +101,30 @@ impl ListNode {
 
     fn end_addr(&self) -> usize {
         self.start_addr() + self.size
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // layoutをListNodeの形式に調整
+        let (size, align) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        if let Some((region, alloc_start)) = allocator.find_region(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("overflow");
+            let excess_size = region.end_addr() - alloc_end;
+            if excess_size > 0 {
+                allocator.add_free_region(alloc_end, excess_size);
+            }
+            alloc_start as *mut u8
+        } else {
+            ptr::null_mut()
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        // layoutをListNodeの形式に調整
+        let (size, _) = LinkedListAllocator::size_align(layout);
+        self.lock().add_free_region(ptr as usize, size)
     }
 }
