6e890835fde1843a8c6c9650397d1b58ac7bb26e
keyem4251 <kisk.emura@gmail.com>
2022-10-02 11:29:50 +0900

    implmenet fixed size allocator fn

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,3 +1,7 @@
+use super::Locked;
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+
 const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 
 struct ListNode {
@@ -23,5 +27,29 @@ impl FixedSizeBlockAllocator {
     // 呼び出し側で有効なメモリの範囲を指定する必要があるためunsafe
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
+    }
+
+    // fallback allocatorを使用して割当を行う
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
+    }
+}
+
+// 受け取ったlayoutを満たすブロックサイズを返す
+fn list_index(layout: &Layout) -> Option<usize> {
+    let required_block_size = layout.size().max(layout.align());
+    BLOCK_SIZES.iter().position(|&s| s >= required_block_size)
+}
+
+unsafe impl GlobalAlloc for Locked<FixedSizeBlockAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        todo!();
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        todo!();
     }
 }
