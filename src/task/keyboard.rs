dbe1a8da5495bba66bb0c1d3aa890928f629ff5d
keyem4251 <kisk.emura@gmail.com>
2022-10-11 16:43:26 +0900

    imple scancode stream

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,8 +1,12 @@
+use core::{pin::Pin, task::{Context, Poll}};
+
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println;
+use futures_util::task::AtomicWaker;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+static WAKER: AtomicWaker = AtomicWaker::new();
 
 // キーボード割り込みハンドラから呼び出される
 pub(crate) fn add_scancode(scancode: u8) {
@@ -10,9 +14,49 @@ pub(crate) fn add_scancode(scancode: u8) {
         if let Err(_) = queue.push(scancode) {
             println!("WARNING: scancode queue full; dropping keyboard input");
             // スキャンコードキューがいっぱいのため、キーボード入力を取りこぼしています
+        } else {
+            WAKER.wake();
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
         // スキャンコードキューが初期化されていません
+    }
+}
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100)).expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
+pub trait Stream {
+    type Item;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>>;
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let queue = SCANCODE_QUEUE.try_get().expect("not initialized");
+        
+        if let Ok(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+
+        WAKER.register(cx.waker());
+        match queue.pop() {
+            Ok(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            },
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
     }
 }
